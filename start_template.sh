95b3665500206650cbf547976be3c00420d9154f
Jaxparrow <36300128+Jackandrichardjunior@users.noreply.github.com>
2020-07-04 20:10:49 +0530

    Menu with 4 options

--- start_template.sh
+++ start_template.sh
@@ -18,14 +18,14 @@ echo "
 Author="Yourname"
 # This will be displayed in the About section
 
-ExtensionName="Sample"
+ExtensionName="Extension"
 # This will be Outputted in the figlets and about section
 
-RunAfterClick="yes"
+RunAfterClick="no"
 # Set this to "yes", if you want to start an action suddenly when a key is pressed. If you set this to "no". The User needs to press Enter Key after inputting
 # Use the functions to execute scripts that you made/or that you have from a Open-Source project
 
-InputBox="Your Choice: "
+InputBox="Your Choice : "
 # This the text to be shown before inputting text (e.g Your Option >, Choice :
 
 
@@ -33,23 +33,33 @@ InputBox="Your Choice: "
 function optone() 
 {
 
-echo "You chose Option 1"
-echo " I am from SupremeGamers"
+echo "Enabling Bypass"
+sleep 1
+echo "Enabled Bypass"
 # Remove the above line and Place the code to be executed when the user selects Option 1
 
 }
 function opttwo()
 {
 
-echo "You Chose Option 2"
+echo "Disabled Bypass"
+
 # Remove the above line and Place the code to be executed when the user selects Option 2
 
 }
 
-function optthree()
+function helpmenu()
 {
+clear
+figlet "${ExtensionName}"
+echo " By ${YELLOW}${Author}${RC} "
+echo "
+"
+echo "
 
-echo "You chose option 3"
+Put some information about your extension
+
+"
 # Remove the above line and Place the code to be executed when the user selects Option 3
 
 }
@@ -63,11 +73,10 @@ MainMenu() {
 clear
 figlet "${ExtensionName}"
 echo " By ${YELLOW}${Author}${RC} "
-echo "Welcome to my Sample Script"
 echo "
 "
-echo "[1] Enable Settings"
-echo "[2] Disable Settings"
+echo "[1] Enable"
+echo "[2] Disable"
 echo "[3] Help"
 echo "[4] Exit"
 echo "
@@ -106,7 +115,7 @@ MainMenu
 3)
 # Define this function at the start
 printspace
-optthree
+helpmenu
 printspace
 echo "${GREEN}++ Press any Key to goto Main Menu${RC}"
 pausemode
